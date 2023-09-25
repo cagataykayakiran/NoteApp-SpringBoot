dc8058aea20c9f774d4c70b7dcfd920c93e96561
Çağatay Kayakıran <cagataykayakiran@gmail.com>
2023-09-25 04:07:46 +0300

    added database function

--- src/main/java/noteApp/repository/NoteRepository.java
+++ src/main/java/noteApp/repository/NoteRepository.java
@@ -4,6 +4,12 @@ import noteApp.Entitiy.NoteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
 @Repository
 public interface NoteRepository extends JpaRepository<NoteEntity, Long> {
+
+    List<NoteEntity> findByUser_Id(Long id);
+
 }
