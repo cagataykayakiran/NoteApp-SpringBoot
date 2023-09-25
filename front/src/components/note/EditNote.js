e4d423833ec86ef3d31e7f05aac4411e87e1204e
Çağatay Kayakıran <cagataykayakiran@gmail.com>
2023-09-25 04:05:17 +0300

    updated EditNote.js component

--- front/src/components/note/EditNote.js
+++ front/src/components/note/EditNote.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import axios from "axios";
 import {NavLink, useNavigate, useParams} from "react-router-dom";
 
-export default function EditUser() {
+export default function EditNote() {
 
     const [text, setText] = useState("")
     const [content, setContent] = useState("")
