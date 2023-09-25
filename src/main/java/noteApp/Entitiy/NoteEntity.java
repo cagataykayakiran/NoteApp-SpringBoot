b6aa41719ae3c3572de065cf99087c5e121064fb
Çağatay Kayakıran <cagataykayakiran@gmail.com>
2023-09-25 04:01:30 +0300

    added userid foreign key

--- src/main/java/noteApp/Entitiy/NoteEntity.java
+++ src/main/java/noteApp/Entitiy/NoteEntity.java
@@ -1,22 +1,25 @@
 package noteApp.Entitiy;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
+@Builder
 public class NoteEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String content;
     private String text;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userid", nullable = false)
+    private User user;
 }
