ff6e1d096899ffa16b46158c96dc87e5708f81a1
cagataykayakiran <cagataykayakiran@gmail.com>
2023-09-29 02:50:40 +0300

    added service annotation

--- src/main/java/noteApp/repository/UserRepository.java
+++ src/main/java/noteApp/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package noteApp.repository;
 
-import noteApp.Entitiy.User;
+import noteApp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
