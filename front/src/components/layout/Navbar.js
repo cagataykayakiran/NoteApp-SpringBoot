eb3cf9006654011d9bba94e9914deba3504a5418
cagataykayakiran <cagataykayakiran@gmail.com>
2023-09-29 03:42:01 +0300

    added user text

--- front/src/components/layout/Navbar.js
+++ front/src/components/layout/Navbar.js
@@ -16,7 +16,9 @@ export default function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container">
-                    <NavLink className={"btn btn-primary "} to={"/save"}>Add Note</NavLink>
+                    <div style={{ display: 'flex' }}>
+                        <NavLink className={"btn btn-primary "} to={"/save"}>Add Note</NavLink>
+                    </div>
                     <div className="container-fluid">
                         <a className="navbar-brand" href="#">Note List</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -30,7 +32,8 @@ export default function Navbar() {
                             user == null ?
                                 <NavLink onClick={onClick} className={"btn btn-primary"} to={"/login"}>Login/Register</NavLink>
                         :
-                                <div style={{ display: 'flex' }}>
+                                <div style={{ display: 'flex', alignItems: 'center' }}>
+                                    <span className="me-5 text-white">Hoş geldiniz, {user.username}!</span>
                                     <NavLink onClick={onClick} className={"btn btn-primary"} style={{ marginRight: '10px' }} to={"/profile"}>Profile</NavLink>
                                     <NavLink onClick={onClick} className={"btn btn-primary"} to={"/home"}>Logout</NavLink>
                                 </div>
