1de52830a728e811c99d692f943cc7c4d498d4c3
cagataykayakiran <cagataykayakiran@gmail.com>
2023-10-11 03:21:28 +0300

    deleted space area

--- src/main/java/noteApp/NoteAppApplication.java
+++ src/main/java/noteApp/NoteAppApplication.java
@@ -4,13 +4,10 @@ package noteApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class NoteAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoteAppApplication.class, args);
 	}
-
-
 }
