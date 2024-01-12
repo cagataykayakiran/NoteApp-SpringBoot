a0dfee7500c619b83bf8e58f206f302a6a1f625f
cagataykayakiran <cagataykayakiran@gmail.com>
2024-01-12 03:47:20 +0300

    move from repository package

--- src/main/java/noteApp/repository/TokenRepository.java
+++ src/main/java/noteApp/repository/TokenRepository.java
@@ -1,5 +1,6 @@
-package noteApp.token;
+package noteApp.repository;
 
+import noteApp.token.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
